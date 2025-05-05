a36788ab6e00056ec4aa0d8cd33ebdf235f0f15e
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-05-05 19:22:51 +0300

    Create Movies model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+  name = models.CharField(max_length=200)
+  rating = models.FloatField(default=0)
+
+  def __str__(self):
+    return self.name
