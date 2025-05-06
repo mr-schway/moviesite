163cddb2524b0e7019b1f9950b2fdfd9885928ef
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-05-06 17:05:44 +0300

    List movies

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from movies.models import Movies
 
 # Create your views here.
+
+def listMovies(request):
+  movie_objects = Movies.objects.all()
+  return render( request, 'movies/list_movies.html', {'movie_objects': movie_objects } )
