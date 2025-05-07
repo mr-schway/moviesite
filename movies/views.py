77d2cd040f6663694d74646a8f657ff6f1582230
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-05-07 21:01:25 +0300

    Show movies with pagination

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from movies.models import Movies
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def listMovies(request):
   movie_objects = Movies.objects.all()
+
+
+  paginator = Paginator(movie_objects, 4)
+  page = request.GET.get('page')
+  movie_objects = paginator.get_page(page)
+
   return render( request, 'movies/list_movies.html', {'movie_objects': movie_objects } )
